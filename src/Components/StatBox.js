df005beabecdc05c1ec760de055a4bfcf0631f14
Adrian Rosales <adrian.e.rosales@gmail.com>
2020-10-25 19:18:06 -0700

    reduced clutter in StatBox component and turned it into a functional component

--- src/Components/StatBox.js
+++ src/Components/StatBox.js
@@ -1,49 +1,8 @@
 import React from 'react'
 
 
-class StatBox extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      turnCollection: []
-    }
-  }
-
-  componentDidUpdate = (prevProps) => {
-    if(this.props.turnCounter != prevProps.turnCounter) {
-    let arr = this.state.turnCollection
-    const rL = this.props.roombaLocation
-
-    let turnCollectionObject = {
-      turn: this.props.turnCounter,
-      roombaLocation: rL,
-      action: this.props.actionLog[this.props.actionLog.length - 1],
-      totalDirtCollected: this.props.dirtCollected,
-      totalWallHits: this.props.bumps
-    }
-    arr.push(turnCollectionObject)
-
-    this.setState({ turnCollection: arr })
-    }
-  }
- 
-  
-
-  render() {
-
-    const turnRows = this.state.turnCollection.map((el, idx) => {
-      return(
-        <tr>
-          <td>{ el.turn }</td>
-          <td>{ el.roombaLocation }</td>
-          <td>{ el.action }</td>
-          <td>{ el.totalDirtCollected }</td>
-          <td>{ el.totalWallHits }</td>
-        </tr>
-      )
-    })
-
-    const dirtLocations = this.props.dirtLocations.map((el, idx) => {
+function StatBox(props) {
+    const dirtLocations = props.dirtLocations.map((el, idx) => {
       return (
         <li key={idx}>{JSON.stringify(el)}</li>
       )
@@ -52,17 +11,16 @@ class StatBox extends React.Component {
     return (
       <div id="statBoxDiv">
         <h1>Stat Box</h1>
-        <p>Total Movements: {this.props.movementCounter}</p>
-        <p>Dirt Collected: {this.props.dirtCollected}</p>
+        <p>Total Movements: {props.movementCounter}</p>
+        <p>Dirt Collected: {props.dirtCollected}</p>
 
         <ul id="dirtLocationsUL">
         <p>Dirt Locations:</p>
           {dirtLocations}
         </ul>
-        <p>Bumps: {this.props.bumps}</p>
+        <p>Bumps: {props.bumps}</p>
       </div>
     )
-  }
 }
 
 export default StatBox
